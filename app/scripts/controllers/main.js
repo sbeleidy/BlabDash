3e1a5544413d335c00fc5cde8035126cdc4f88b6
Saad Elbeleidy <sbeleidy90@gmail.com>
2016-01-13 20:51:16 +0200

    Update main view to bind to updated firebase

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,12 +9,5 @@
  */
 angular.module('testFireApp')
   .controller('MainCtrl', function ($scope, Ref, $firebaseArray) {
-  	$scope.today = $firebaseArray(Ref.child('stats').limitToLast(1));
-  	$scope.today.$loaded().then(function(){
-	  	$scope.hours = 132;
-	  	$scope.newUsers = Object.keys($scope.today[0].new_users).length;
-	    $scope.averageMins = 45;
-	    $scope.onCamUsers = Object.keys($scope.today[0].on_cam).length;
-	    $scope.blabs = Object.keys($scope.today[0].blabs).length;
-  	});
+  	$scope.today = $firebaseArray(Ref.child('summary').limitToLast(1));
   });
