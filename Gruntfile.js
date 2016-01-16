4cd50d17ab0a109c316b9b1ee72e276bf8312df7
Saad Elbeleidy <sbeleidy90@gmail.com>
2016-01-16 17:11:58 +0200

    Add successful test, and setup node packages and grunt file for successful list build

--- Gruntfile.js
+++ Gruntfile.js
@@ -426,7 +426,7 @@ module.exports = function (grunt) {
     'concat',
     'ngAnnotate',
     'copy:dist',
-    'cdnify',
+    // 'cdnify',
     'cssmin',
     'uglify',
     'filerev',
--- test/karma.conf.js
+++ test/karma.conf.js
@@ -35,9 +35,9 @@ module.exports = function(config) {
       'bower_components/mockfirebase/browser/mockfirebase.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
-      // "app/scripts/**/*.js",
-      // "test/mock/**/*.js",
-      // "test/spec/**/*.js"
+      "app/scripts/**/*.js",
+      "test/mock/**/*.js",
+      "test/spec/**/*.js"
     ],
 
     // list of files / patterns to exclude
